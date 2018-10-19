bcf57c069b082da0344e660a1d2d27b09dd0003c
Identity Network <xid.identity.project@gmail.com>
2018-10-19 07:04:25 -0400

    Update effigysilver.sol

--- contracts/effigysilver.sol
+++ contracts/effigysilver.sol
@@ -4,6 +4,7 @@ import "./effigyiron.sol";
 import "./erc721.sol";
 
 contract EffigySilver is EffigyIron, ERC721 {
+mapping (uint => address) effigyApprovals;
  function balanceOf(address _owner) public view returns (uint256 _balance) {
     return ownerEffigyCount[_owner];
   }
@@ -11,16 +12,29 @@ contract EffigySilver is EffigyIron, ERC721 {
   function ownerOf(uint256 _tokenId) public view returns (address _owner) {
     return effigyToOwner[_tokenId];
   }
+  
+  
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerEffigyCount[_to]++;
+    ownerEffigyCount[_from]--;
+    effigyToOwner[_tokenId] = _to;
+    Transfer(_from, _to, _tokenId);
+  }
 
-  function transfer(address _to, uint256 _tokenId) public {
+  function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    _transfer(msg.sender, _to, _tokenId);
 
   }
 
-  function approve(address _to, uint256 _tokenId) public {
+   function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+    effigyApprovals[_tokenId] = _to;
+    Approval(msg.sender, _to, _tokenId);
 
   }
 
   function takeOwnership(uint256 _tokenId) public {
-
+ require(effigyApprovals[_tokenId] == msg.sender);
+    address owner = ownerOf(_tokenId);
+    _transfer(owner, msg.sender, _tokenId);
   }
 }
