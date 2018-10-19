73085ff533398110684386e0b047fc9087400016
Identity Network <xid.identity.project@gmail.com>
2018-10-19 06:23:12 -0400

    Create effigyiron.sol

---
+++ contracts/effigyiron.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.19;
+import "./effigybronze.sol";
+contract EffigyIron is EffigyBronze {
+uint randNonce = 0;
+uint attackVictoryProbability = 70;
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
+  function attack(uint _effigyId, uint _targetId) external {
+  }
+}
